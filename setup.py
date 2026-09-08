234b7230c37d350582ea1f9c1c85c468142cada1
Himaja Busam <himajabusam2@gmail.com>
2026-09-08 16:05:04 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    #This func return list of requriments
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace("\n","") for req in requirments]
+
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+
+    return requirments 
+
+setup(
+name="mlproject",
+version='0.0.1',
+author='Himaja',
+author_email='himajabusam2@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirement.txt')
+
+)
