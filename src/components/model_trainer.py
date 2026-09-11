d22e98715bfa34d874c134497da87c0dcdd0d89f
Himaja Busam <himajabusam2@gmail.com>
2026-09-11 17:46:21 +0530

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,74 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from src.exception import CustomException
+from src.logger import logging  
+
+from src.utils import save_object,evaluate_models
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Logistic Regression": LogisticRegression(),
+                "K-Nearest Neighbors": KNeighborsRegressor(),
+                "Support Vector Machine": SVR(),
+                "XGBoost": XGBRegressor(),
+                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
+                "AdaBoost Classifier": AdaBoostRegressor(),
+            }
+
+            model_report: dict =evaluate_models(x_train=X_train, y_train=y_train, x_test=X_test, y_test=y_test, models=models)
+
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+            if best_model_score < 0.6:
+                raise CustomException("No best model found with score above 0.6")
+
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model,
+            )
+
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            logging.info(f"R2 Score for best model: {r2_square}")  
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, r2_score
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.exception import CustomException
@@ -12,5 +13,27 @@ def save_object(file_path:str,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_models(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            # Train model
+            model.fit(x_train,y_train)
+            y_train_pred=model.predict(x_train)
+            # Predict Testing data
+            y_test_pred=model.predict(x_test)
+
+            # Get model score
+            test_model_score=r2_score(y_test,y_test_pred)
+            train_model_score=r2_score(y_train,y_train_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e,sys)
